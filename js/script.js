3b9c826d55c305fad427d400066e732f83d07bde
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-17 13:38:40 +0200

    Exercise Completed

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const app = new Vue ({
+    el: "#root",
+    data: {
+        // TESTO
+        message: "Welcome to Vue Js",
+        textCSS: "text",
+        // IMMAGINE
+        image: "img/logo-vuejs.png",
+        altImage: "Image Logo VueJs",
+        imageCSS: "image"
+    }
+});
