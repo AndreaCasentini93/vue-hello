5c8434180e666638ab3dc782f90039704851808e
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-17 14:33:24 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const app = new Vue ({
     data: {
         // TESTO
         message: "Welcome to Vue Js",
-        textCSS: "text-vuejs",
+        textCSS: "text",
         // IMMAGINE
         image: "img/logo-vuejs.png",
         altImage: "Image Logo VueJs",
-        imageCSS: "image-vuejs",
+        imageCSS: "image",
         // BACKGROUND
         background: "background-vuejs"
     },
@@ -17,29 +17,29 @@ const app = new Vue ({
                 case "img/logo-vuejs.png":
                     // TESTO
                     this.message = "Welcome to React";
-                    this.textCSS = "text-react";
                     // IMMAGINE
                     this.image = "img/logo-react.png";
                     this.altImage = "Image Logo React";
-                    this.imageCSS = "image-react";
+                    // BACKGROUND
+                    this.background = "background-react";
                     break
                 case "img/logo-react.png":
                     // TESTO
                     this.message = "Welcome to Angular";
-                    this.textCSS = "text-angular";
                     // IMMAGINE
                     this.image = "img/logo-angular.png";
                     this.altImage = "Image Logo Angular";
-                    this.imageCSS = "image-angular";
+                    // BACKGROUND
+                    this.background = "background-angular";
                     break
                 default:
                     // TESTO
                     this.message = "Welcome to Vue Js";
-                    this.textCSS = "text-vuejs";
                     // IMMAGINE
                     this.image = "img/logo-vuejs.png";
                     this.altImage = "Image Logo VueJs";
-                    this.imageCSS = "image-vuejs";
+                    // BACKGROUND
+                    this.background = "background-vuejs";
             }
         }
     }
